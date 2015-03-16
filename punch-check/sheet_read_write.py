933b7dc8954097e9348b2f337de65521da5448c1
sunyijun <sun.yj@30raycom.com>
2015-03-16 12:39:43 +0800

    移动异常sheet的链接列到前面。异常、详细、未排班sheet添加间隔行背景色。

--- punch-check/sheet_read_write.py
+++ punch-check/sheet_read_write.py
@@ -48,7 +48,7 @@ def read_time_cells(sheet, date_mode, time_row, time_col):
     time_tuple = xlrd.xldate_as_tuple(sheet.cell(time_row, time_col).value, date_mode)
     return time(*time_tuple[3:])
 
-
+# 颜色表详见Style.py
 YELLOW_BG_PATTERN = xlwt.Pattern()
 YELLOW_BG_PATTERN.pattern = xlwt.Pattern.SOLID_PATTERN
 YELLOW_BG_PATTERN.pattern_fore_colour = 5  # May be: 8 through 63. 0 = Black, 1 = White, 2 = Red, 3 = Green, 4 = Blue, 5 = Yellow, 6 = Magenta, 7 = Cyan, 16 = Maroon, 17 = Dark Green, 18 = Dark Blue, 19 = Dark Yellow , almost brown), 20 = Dark Magenta, 21 = Teal, 22 = Light Gray, 23 = Dark Gray, the list goes on...
@@ -56,6 +56,10 @@ YELLOW_BG_PATTERN.pattern_fore_colour = 5  # May be: 8 through 63. 0 = Black, 1 
 MAGENTA_BG_PATTERN = xlwt.Pattern()
 MAGENTA_BG_PATTERN.pattern = xlwt.Pattern.SOLID_PATTERN
 MAGENTA_BG_PATTERN.pattern_fore_colour = 6  # May be: 8 through 63. 0 = Black, 1 = White, 2 = Red, 3 = Green, 4 = Blue, 5 = Yellow, 6 = Magenta, 7 = Cyan, 16 = Maroon, 17 = Dark Green, 18 = Dark Blue, 19 = Dark Yellow , almost brown), 20 = Dark Magenta, 21 = Teal, 22 = Light Gray, 23 = Dark Gray, the list goes on...
+
+GRAY_BG_PATTERN = xlwt.Pattern()
+GRAY_BG_PATTERN.pattern = xlwt.Pattern.SOLID_PATTERN
+GRAY_BG_PATTERN.pattern_fore_colour = 22  # May be: 8 through 63. 0 = Black, 1 = White, 2 = Red, 3 = Green, 4 = Blue, 5 = Yellow, 6 = Magenta, 7 = Cyan, 16 = Maroon, 17 = Dark Green, 18 = Dark Blue, 19 = Dark Yellow , almost brown), 20 = Dark Magenta, 21 = Teal, 22 = Light Gray, 23 = Dark Gray, the list goes on...
 
 LINK_FONT = xlwt.Font()
 LINK_FONT.name = 'Times New Roman'
@@ -66,7 +70,10 @@ LINK_FONT.colour_index = 4  # May be: 8 through 63. 0 = Black, 1 = White, 2 = Re
 def write_details_sheet_row(row, name, department, punch_datetime, punch_type,
                             link_row, timeInfoSheet=False, noPlanSheet=False):
     style = Style.default_style
+    origin_pattern = style.pattern
     origin_font = style.font
+    if row % 2 == 0:
+        style.pattern = GRAY_BG_PATTERN
     outputDetailsSheet.write(row, 0, name)
     outputDetailsSheet.write(row, 1, department)
     outputDetailsSheet.write(row, 2, str(punch_datetime))
@@ -75,11 +82,12 @@ def write_details_sheet_row(row, name, department, punch_datetime, punch_type,
     style.font = LINK_FONT
     if timeInfoSheet:
         outputDetailsSheet.write(row, 5, xlwt.Formula(
-            'HYPERLINK("#TimeInfo!A' + str(link_row) + '","TimeInfo...")'))
+            'HYPERLINK("#TimeInfo!A' + str(link_row) + '")'))
     if noPlanSheet:
         outputDetailsSheet.write(row, 5, xlwt.Formula(
-            'HYPERLINK("#NotPlan!A' + str(link_row) + '","NotPlan...")'))
+            'HYPERLINK("#NotPlan!A' + str(link_row) + '")'))
     style.font = origin_font
+    style.pattern = origin_pattern
     return row + 1
 
 
@@ -96,7 +104,7 @@ def write_by_date_sheet_row(row, name, date, punch_in_datetime, punch_out_dateti
         outputByDateSheet.write(row, 6, msg)
         style.font = LINK_FONT
         outputByDateSheet.write(row, 7, xlwt.Formula(
-            'HYPERLINK("#Details!A' + str(link_details_sheet_row) + '","Details...")'))
+            'HYPERLINK("#Details!A' + str(link_details_sheet_row) + '")'))
         style.font = origin_font
     outputByDateSheet.write(row, 0, name)
     outputByDateSheet.write(row, 1, str(date))
@@ -111,7 +119,7 @@ def write_by_date_sheet_row(row, name, date, punch_in_datetime, punch_out_dateti
     style.num_format_str = 'h:mm'
     rowNum = str(row + 1)
     outputByDateSheet.write(row, 4, xlwt.Formula(
-        #IF(D?*C?,IF(D?<C?,D?+"24:00:00",D?)-C?),"")
+        # IF(D?*C?,IF(D?<C?,D?+"24:00:00",D?)-C?),"")
         'IF(D' + rowNum + '*C' + rowNum + ',IF(D' + rowNum + '<C' + rowNum + ',D' + rowNum +
         '+"24:00:00",D' + rowNum + ')-C' + str(row + 1) + ',"")'))
     style.num_format_str = 'General'
@@ -122,30 +130,39 @@ def write_by_date_sheet_row(row, name, date, punch_in_datetime, punch_out_dateti
 
 def write_final_sheet_row(row, name, department, leave_start, leave_end, type, link_exception_row):
     style = Style.default_style
+    origin_pattern = style.pattern
     origin_font = style.font
+    if row % 2 == 0:
+        style.pattern = GRAY_BG_PATTERN
     outputFinalSheet.write(row, 0, row)
     outputFinalSheet.write(row, 1, name)
     outputFinalSheet.write(row, 2, department)
+    outputFinalSheet.write(row, 3, '')
     outputFinalSheet.write(row, 4, str(leave_start))
     outputFinalSheet.write(row, 5, str(leave_end))
     outputFinalSheet.write(row, 6, type)
     style.font = LINK_FONT
-    outputFinalSheet.write(row, 10, xlwt.Formula(
-        'HYPERLINK("#TimeInfo!A' + str(link_exception_row) + '","TimeInfo...")'))
+    outputFinalSheet.write(row, 7, xlwt.Formula(
+        'HYPERLINK("#TimeInfo!A' + str(link_exception_row) + '")'))
     style.font = origin_font
+    style.pattern = origin_pattern
     return row + 1
 
 
 def write_no_plan_sheet_row(row, name, department, link_details_sheet_row):
     style = Style.default_style
+    origin_pattern = style.pattern
     origin_font = style.font
+    if row % 2 == 0:
+        style.pattern = GRAY_BG_PATTERN
     outputNoPlanSheet.write(row, 0, row)
     outputNoPlanSheet.write(row, 1, name)
     outputNoPlanSheet.write(row, 2, department)
     style.font = LINK_FONT
     outputNoPlanSheet.write(row, 3, xlwt.Formula(
-        'HYPERLINK("#Details!A' + str(link_details_sheet_row) + '","Details...")'))
+        'HYPERLINK("#Details!A' + str(link_details_sheet_row) + '")'))
     style.font = origin_font
+    style.pattern = origin_pattern
     return row + 1
 
 
@@ -159,10 +176,10 @@ outputFinalSheet.col(3).width = 256 * 18
 outputFinalSheet.col(4).width = 256 * 24
 outputFinalSheet.col(5).width = 256 * 24
 outputFinalSheet.col(6).width = 256 * 14
-outputFinalSheet.col(7).width = 256 * 12
-outputFinalSheet.col(8).width = 256 * 15
-outputFinalSheet.col(9).width = 256 * 12
-outputFinalSheet.col(10).width = 256 * 15
+outputFinalSheet.col(7).width = 256 * 15
+outputFinalSheet.col(8).width = 256 * 12
+outputFinalSheet.col(9).width = 256 * 15
+outputFinalSheet.col(10).width = 256 * 12
 outputFinalSheet.write(0, 0, '序号')
 outputFinalSheet.write(0, 1, '姓名')
 outputFinalSheet.write(0, 2, '部门')
@@ -170,10 +187,10 @@ outputFinalSheet.write(0, 3, '岗位')
 outputFinalSheet.write(0, 4, '起假时间')
 outputFinalSheet.write(0, 5, '截止时间')
 outputFinalSheet.write(0, 6, '假别')
-outputFinalSheet.write(0, 7, '时间段')
-outputFinalSheet.write(0, 8, '请假时间（小时）')
-outputFinalSheet.write(0, 9, '状态')
-outputFinalSheet.write(0, 10, '排班链接')
+outputFinalSheet.write(0, 7, '排班链接')
+outputFinalSheet.write(0, 8, '时间段')
+outputFinalSheet.write(0, 9, '请假时间（小时）')
+outputFinalSheet.write(0, 10, '状态')
 
 outputByDateSheet = outputData.add_sheet('TimeInfo')
 outputByDateSheet.col(0).width = 256 * 12
@@ -194,7 +211,7 @@ outputByDateSheet.write(0, 6, '异常信息')
 outputByDateSheet.write(0, 7, '详细链接')
 
 outputDetailsSheet = outputData.add_sheet('Details')
-#name, department, punch_datetime, punch_type,link_exception_sheet_row
+# name, department, punch_datetime, punch_type,link_exception_sheet_row
 outputDetailsSheet.col(0).width = 256 * 12
 outputDetailsSheet.col(1).width = 256 * 18
 outputDetailsSheet.col(2).width = 256 * 15
