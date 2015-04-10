d032217e61fa34788e637c6a8e56388ccfeeceec
yijun.sun <dusunjun@126.com>
2015-04-10 10:19:21 +0800

    整理结构，修改打包批处理。

--- punch-check/build.bat
+++ punch-check/build.bat
@@ -1,2 +1,12 @@
+@echo off
+rmdir /S/Q punch-check
 python update-setup.py py2exe
 python punch-setup.py py2exe
+rename dist punch-check
+xcopy config punch-check\config\
+rem java -jar ..\tools\Markdown2HTML-1.0-SNAPSHOT.jar docs\参考手册.md -out punch-check\参考手册.html
+rem java -jar ..\tools\Markdown2HTML-1.0-SNAPSHOT.jar docs\使用说明.md -out punch-check\使用说明.html
+copy docs\参考手册.html punch-check\
+copy docs\使用说明.html punch-check\
+copy docs\changelist.txt punch-check\
+pause
